f46ab875b3dff52d1939265e24380b1a5599f2bd
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-02-24 15:51:19 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,22 @@
+class PigLatinizer
+ 
+  
+  def piglatinize(word)
+    #word.split("").join
+ # if word == /\A[^aeiou]/
+ # return 
+  word.split("").join << "ay"
+  #.shift << "ay"
+  #end   
+  
+end
+#end 
+#word.split("").shift << "ay"
+    #sort_by{rand}.join + "ay"
+      
+      #word.start_with?('a','e','i','o','u')
+    #word.split("").sort_by{rand}.join + "ay"
+end 
+# end 
+ # end 
+  
