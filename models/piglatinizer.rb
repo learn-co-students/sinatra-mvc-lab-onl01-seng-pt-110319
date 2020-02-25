e968bbaba31d3da5783172ab20934f6a781a1207
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-02-25 14:06:22 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,22 +1,25 @@
 class PigLatinizer
- 
   
-  def piglatinize(word)
-    #word.split("").join
- # if word == /\A[^aeiou]/
- # return 
-  word.split("").join << "ay"
-  #.shift << "ay"
-  #end   
-  
+  def piglatinize(string)
+    a = string.split(" ")
+    b = a.map {|word| piglatinize_word(word)}
+    b.join(" ")
 end
-#end 
-#word.split("").shift << "ay"
-    #sort_by{rand}.join + "ay"
-      
-      #word.start_with?('a','e','i','o','u')
-    #word.split("").sort_by{rand}.join + "ay"
-end 
-# end 
- # end 
   
+  def piglatinize_word(word)
+    first_letter = word[0].downcase
+    if ["a", "e", "i", "o", "u"].include?(first_letter)
+        "#{word}way"
+    else
+      consonants = []
+      consonants << word[0]
+        if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+          consonants << word[1]
+          if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+            consonants << word[2]
+          end
+        end
+      "#{word[consonants.length..-1] + consonants.join + "ay"}"
+    end
+    end 
+  end 
